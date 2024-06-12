c2b30ae2e0ecaf08a241ffe924e9a4d1c60b7abe
diana.carjan <carjan.diana@gmail.com>
2024-06-12 19:21:27 +0300

    Add verify script

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,86 @@
 import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
-import { generatePrimes } from "./utils";
+import { generatePrimes, generatePrimesSequentially } from "./utils";
+import os from "os";
 
-const main = (workersCount = 8) => {
-  const min = 2;
-  const max = 1e7;
-  
-  const range = Math.ceil((max - min) / workersCount);
-  const workers = [];
+export const parallelPrimes = (
+  workersCount: number,
+  min: number,
+  range: number
+): Promise<number[]> => {
+  return new Promise((resolve, reject) => {
+    const slice = Math.ceil(range / workersCount);
 
-  for (let i = 0; i < workersCount; i++) {
-    const start = min + i * range;
-    const end = i === workersCount - 1 ? max : start + range;
+    const workers = [];
+    let primes: number[] = [];
+    let completed = 0;
 
-    const worker = new Worker(__filename, {
-      workerData: { start, end },
-    });
+    for (let i = 0; i < workersCount; i++) {
+      const start = min + i * slice;
+      const end = i === workersCount - 1 ? min + range : start + slice;
 
-    worker.on("message", (msg) => {
-      console.log(`Worker ${i} finished processing`, msg);
-    });
-    worker.on("error", console.error);
-    worker.on("exit", (code) => {
-      if (code !== 0)
-        console.error(new Error(`Worker stopped with exit code ${code}`));
-    });
-    workers.push(worker);
-  }
+      const worker = new Worker(__filename, {
+        workerData: { start, end },
+      });
 
-  let primes: number[] = [];
-  let completed = 0;
+      worker.on("message", (primesSegment: number[]) => {
+        console.log(`Worker ${i} finished processing`, primesSegment);
 
-  workers.forEach((worker) => {
-    worker.on("message", (primesSegment) => {
-      primes = primes.concat(primesSegment);
-      completed++;
+        primes = primes.concat(primesSegment);
+        completed++;
 
-      if (completed === workersCount) {
-        console.log(`Total count of prime numbers: ${primes.length}`);
-      }
-    });
+        if (completed === workersCount) {
+          primes.sort((a, b) => a - b);
+          resolve(primes);
+        }
+      });
+
+      worker.on("error", reject);
+      worker.on("exit", (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+
+      workers.push(worker);
+    }
   });
 };
 
-if (isMainThread) {
-  main();
-} else {
-  const { start, end } = workerData;
-  const primes = generatePrimes(start, end);
-  parentPort!.postMessage(primes);
-}
+const main = (verify = false, min = 2, range = 1e7) => {
+  const workersCount = process.argv[2]
+    ? parseInt(process.argv[2], 10)
+    : os.cpus().length;
+
+  if (isMainThread) {
+     parallelPrimes(workersCount, min, range)
+      .then((primes) => {
+        console.log(`Total count of prime numbers: ${primes.length}`);
+        if (verify) {
+          const sequentialResult = generatePrimesSequentially(min, range);
+          console.log("Parallel primes " + primes);
+          console.log("Seq primes " + sequentialResult);
+
+          if (
+            sequentialResult.filter((item) => primes.indexOf(item) < 0).length >
+            0
+          ) {
+            console.error("Arrays are not the same");
+          } else {
+            console.log("Successful verify");
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+  } else {
+    const { start, end } = workerData;
+    const primes = generatePrimes(start, end);
+    parentPort!.postMessage(primes);
+  }
+};
+
+const verify = process.argv[3] === "true" || false;
+const min = parseInt(process.argv[4]) || 2;
+const range = parseInt(process.argv[5]) || 1e7;
+
+main(verify, min, range);
--- src/utils.ts
+++ src/utils.ts
@@ -14,9 +14,40 @@ const isPrime = (element: number): boolean => {
   if (element % 2 === 0 || element % 3 === 0) return false;
 
   const end = Math.sqrt(element);
-  
+
   for (let i = 5; i <= end; i += 6)
     if (element % i == 0 || element % (i + 2) == 0) return false;
 
   return true;
+};
+
+// Original function to be tested against
+export const generatePrimesSequentially = (min = 2, range = 1e7) => {
+  const primes: number[] = [];
+
+  const generate = (start: number, range: number) => {
+    let isPrime = true;
+
+    let end = start + range;
+
+    for (let i = start; i < end; i++) {
+      for (let j = min; j < Math.sqrt(end); j++) {
+        if (i !== j && i % j === 0) {
+          isPrime = false;
+
+          break;
+        }
+      }
+
+      if (isPrime) {
+        primes.push(i);
+      }
+
+      isPrime = true;
+    }
+  };
+
+  generate(min, range);
+
+  return primes;
 };
